3c3801ced16d8fbf7bc749b2ddca060762f1a2d9
Ednardo Gomes <ednardo@email.com>
2025-02-25 00:10:13 -0300

    feat: Create component imput.

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,11 @@
 import { ComponentProps } from "react";
 
-interface IconButtonProps extends ComponentProps<'button'> {}
+interface IconButtonProps extends ComponentProps<'button'> { }
 
 export function IconButton(props: IconButtonProps) {
   return (
     <button
-      className="flex justify-between items-center px-5 w-full h-12 bg-gray-500 text-blue font-semibold rounded-xl cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className="p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
       {...props}
     />
   );
